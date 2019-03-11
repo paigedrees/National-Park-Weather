fad18b533e62d83fef73ede01ea9d78f60e06244
Brett Bauereis <brettbauereis@gmail.com>
2019-03-11 11:55:51 -0400

    Recommendations finished

--- 12-Capstone/java/src/main/java/com/techelevator/npgeek/model/Weather.java
+++ 12-Capstone/java/src/main/java/com/techelevator/npgeek/model/Weather.java
@@ -1,7 +1,10 @@
 package com.techelevator.npgeek.model;
 import java.time.LocalDateTime;
 import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 
 public class Weather {
@@ -13,16 +16,73 @@ public class Weather {
 	private String forecast;
 	//private static final DateFormatSymbols dayNames= new DateFormatSymbols();
 	
+	public String getDayName() {
+		
+		return LocalDateTime.now().plusDays(day - 1).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ROOT);
+	}
+	
+	public String getRecommendation() {
+		List<String> toPack = new ArrayList<String>();
+		List<String> toWear = new ArrayList<String>();
+		List<String> warnings = new ArrayList<String>();
+		
+		switch(forecast) {
+		
+		case "snow":
+			toPack.add("snowshoes");
+			break;
+			
+		case "thunderstorms":
+			warnings.add("Seek shelter.");
+			warnings.add("Avoid hiking on exposed ridges.");
+			//Fall through
+			
+		case "rain":
+			toPack.add("rain gear");
+			toWear.add("waterproof shoes");
+			break;
+			
+		case "sunny":
+			toPack.add("sunblock");
+			break;
+		}	
+		
+		
+		if (high > 75) {
+			toPack.add("Extra gallon of Water");
+		}
+		if ( (high - low) > 20) {
+			toWear.add("breathable layers");
+		}
+		if ( low < 20) {
+			warnings.add("Danger of exposure to frigid temperatures.");
+		}
+		
+		StringBuffer result = new StringBuffer();
+		if (toPack.size() > 0) {
+			result.append("We recommend that you pack ");
+			result.append( toPack.stream().collect(Collectors.joining(", ", null, ".\n")) );
+		}
+		if (toWear.size() > 0) {
+			result.append("We recommend that you wear ");
+			result.append( toWear.stream().collect(Collectors.joining(", ", null, ".\n")) );
+		}
+		if (warnings.size() > 0) {
+			result.append("The following warnings are in effect: ");
+			result.append( warnings.stream().collect(Collectors.joining("; ", null, ".\n")) );
+		}
+		
+		
+		return result.toString();
+	}
+	
 	public String getCode() {
 		return code;
 	}
 	public void setCode(String code) {
 		this.code = code;
 	}
-	public String getDayName() {
 	
-		return LocalDateTime.now().plusDays(day - 1).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ROOT);
-	}
 	public void setDay(int day) {
 		this.day = day;
 	}
