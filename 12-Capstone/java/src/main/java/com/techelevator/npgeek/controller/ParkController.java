bd214c07f29ec8c90f10ea14d5322bb94f22670e
Paige Drees <dreespe@miamioh.edu>
2019-03-11 15:43:27 -0400

    Need to fix temperature

--- 12-Capstone/java/src/main/java/com/techelevator/npgeek/controller/ParkController.java
+++ 12-Capstone/java/src/main/java/com/techelevator/npgeek/controller/ParkController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.techelevator.npgeek.model.Park;
 import com.techelevator.npgeek.model.ParkDAO;
+import com.techelevator.npgeek.model.TemperatureChoice;
 
 @Controller
 public class ParkController {
@@ -18,8 +19,10 @@ public class ParkController {
 	
 	@RequestMapping("/home") 
 	public String getHome(ModelMap modelMap){
-		
+		TemperatureChoice tempKey = new TemperatureChoice();
+		tempKey.setScale("C");
 		modelMap.put("parks", parkDao.getAllParks());
+		modelMap.put("temperatureChoice", tempKey);
 		return "homePage";
 	}
 	
